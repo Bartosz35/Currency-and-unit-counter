77e836611d57c5c3d61009d969827181ee452061
Bartosz Arnista <Bartosz.arni@gmail.com>
2020-07-05 07:33:06 +0200

    CurrencyConvertingFirst

---
+++ src/main/java/CurrencyConverter.java
@@ -0,0 +1,24 @@
+/**
+
+ public class CurrencyConverter {
+
+ CurrencyConverter currencyConverter = new BankUaCom(Currency.USD, Currency.EUR);
+
+
+ currencyConverter.convertCurrency(1f);
+
+
+ currencyConverter.convertCurrency(1f,Currency.EUR);
+
+
+ currencyConverter.convertCurrency(1f,Currency.USD,Currency.EUR);
+
+
+ currencyConverter.convertCurrency(1f,Currency.EUR,Currency.USD);
+
+
+
+ }
+
+ */
+
